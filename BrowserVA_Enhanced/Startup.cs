58bb3fe11c6a9438fe5cb776d30e4a3099f376bd
Himanshi Dwivedi <himanshi@viggeby.com>
2019-04-10 13:20:16 +0530

    Add project files.

---
+++ BrowserVA_Enhanced/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BrowserVA_Enhanced.Startup))]
+namespace BrowserVA_Enhanced
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
